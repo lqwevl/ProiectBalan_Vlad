1683aaf87c0d25c2aa6dec8f1efd9a876a4ef052
lqwevl <balan.vlad@elev.cihcahul.md>
2025-04-11 09:13:27 +0300

    Adăugat fișierul script.js în ramura feature3

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const image = document.querySelector("img");
+    if (image) {
+      image.addEventListener("click", function() {
+        alert("Ai clicat pe imagine!");
+      });
+    }
+  });
+  
